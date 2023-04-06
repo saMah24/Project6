6a4247c8958ceb809f4a50dd6400792c3b5aa3ec
mshah2class-qaengineering <mshah2class-qaengineering@localhost>
2023-04-06 14:05:05 -0400

    Initial Commit

---
+++ src/demo6/Test6.java
@@ -0,0 +1,14 @@
+package demo6;
+
+public class Test6 {
+	
+public static void main(String[] args) {
+		
+		System.out.println("GitHub HW6...");
+		
+		System.out.println("Testing Continues6...");
+		
+		
+	}
+
+}
